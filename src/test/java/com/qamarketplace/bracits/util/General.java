efd82d87517e9666ade3ee9ce56201617c3366b5
fahmida.akther <fahmida.himu0904@gmail.com>
2025-06-30 17:44:34 +0600

    Changes

---
+++ src/test/java/com/qamarketplace/bracits/util/General.java
@@ -0,0 +1,5 @@
+package com.qamarketplace.bracits.util;
+
+public class General {
+    final public static int WAIT_TIME = 5000;
+}
---
+++ src/test/java/com/qamarketplace/bracits/util/QaMarketPlaceString.java
@@ -0,0 +1,5 @@
+package com.qamarketplace.bracits.util;
+
+public class QaMarketPlaceString {
+    public static final String LOGIN_TITLE = "Your store. Login";
+}
