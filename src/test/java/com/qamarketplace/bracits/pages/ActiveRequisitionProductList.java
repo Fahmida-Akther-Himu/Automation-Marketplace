ace37aff79bfcc3d3b5c10b8aa96408ddc0ff46c
fahmida.akther <fahmida.himu0904@gmail.com>
2025-06-30 17:53:46 +0600

    Changes

---
+++ src/test/java/com/qamarketplace/bracits/pages/ActiveRequisitionProductList.java
@@ -0,0 +1,21 @@
+package com.qamarketplace.bracits.pages;
+
+import com.qamarketplace.bracits.pages.base.BasePage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class ActiveRequisitionProductList extends BasePage {
+    public ActiveRequisitionProductList(WebDriver driver) {
+        super(driver);
+    }
+
+    public boolean hasLogoutLink() {
+
+        return getWebElements(By.linkText("Log out")).size() > 0;
+    }
+
+    public ActiveRequisitionProductList clickAddToCartBtn() {
+        getWebElement(By.cssSelector("button.button-1.cart-button")).click();
+        return getInstance(ActiveRequisitionProductList.class);
+    }
+}
